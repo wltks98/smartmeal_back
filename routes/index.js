d4443cd5198f00f76b4dd24d77e34a729b83a93a
wltks98 <alwhs1230@gmail.com>
2021-11-23 19:42:49 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
+const models = require("../models");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//필터 디폴트값
+router.get("/filter_default",async function(req,res,next){
+
+  let session=req.session;
+
+
+  let user=models.user.findOne({
+    where: {user_id : session.user_id}
+  })
+  .catch( err => { 
+    console.log(err);
+    return res.send({msg:false})
+  });
+
+  let inbody_type=user.inbody_type;
+
+  let min_tan,min_dan,min_ji;
+  let max_tan,max_dan,max_ji;
+  let max_cal,min_cal;
+
+  if (inbody_type=='저체중 허약형'){
+    
+    min_tan=3;
+    max_tan=30;
+
+    min_dan=10;
+    max_dan=30;
+
+    min_ji=0;
+    max_ji=20;
+
+
+    min_cal=0;
+    max_cal=300;
+  }
+  else if(inbody_type=='과체중 비만형'){
+
+    min_tan=0;
+    max_tan=12;
+
+    min_dan=10;
+    max_dan=30;
+
+    min_ji=0;
+    max_ji=6;
+
+    min_cal=0;
+    max_cal=150;
+  }
+  else{
+    min_tan=3;
+    max_tan=20;
+
+    min_dan=10;
+    max_dan=30;
+
+    min_ji=0;
+    max_ji=15;
+
+    min_cal=0;
+    max_cal=200;
+  }
+
+  
+
+  res.json({
+    min_tan:min_tan,
+    min_dan:min_dan,
+    min_ji:min_ji,
+    max_tan:max_tan,
+    max_dan:max_dan,
+    max_ji:max_ji,
+    max_cal:max_cal,
+    min_cal:min_cal
+  })
+
+  
+
+})
+
 
 module.exports = router;
