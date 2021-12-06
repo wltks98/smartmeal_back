56543f7e8b1428aac11bb5c22bc74bed7e04ea17
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2021-12-06 11:12:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 const models = require("./models/index.js");
 const session = require('express-session');
 
@@ -37,11 +38,13 @@ app.use(session({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jsx');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
